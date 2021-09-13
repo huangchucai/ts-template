2975661c6b5b03039c6298ffab7b8aa094c5966a
huangchucai <425205839@qq.com>
2021-09-13 23:33:09 +0800

    添加eslint

--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,11 @@ import React from "react";
 // import * as React from 'react';
 // import * as ReactDOM from 'react-dom';
 
-import  ReactDOM  from "react-dom";
+import ReactDOM from "react-dom";
 import 'antd/dist/antd.css';
 import App from './App'
-    const a = require('./test')
 
 ReactDOM.render(
-    <App toWhat="World by jsx" />,
+    <App toWhat="World by jsx"/>,
     document.getElementById("app")
 )
